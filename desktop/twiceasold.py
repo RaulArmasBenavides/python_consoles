a854cd82f71ccdedfa76daea7eb4add9e4738c3d
Raúl Armas <raularmasbx@gmail.com>
2024-03-26 01:44:35 -0500

    RMAB updates

---
+++ desktop/twiceasold.py
@@ -0,0 +1,8 @@
+# Calculate how many years ago the fatehr was twice as old as his son  ( or in how many years he will be twice as old)
+def twice_as_old(dad_year_old,son_years_old):
+  return abs(dad_year_old - (2*son_years_old))
+    
+
+print(twice_as_old(50,26))
+      
+    
